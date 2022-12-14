2156092e6807721b261012ae995ad6239e727154
DevPCW <rorndksk123@naver.com>
2022-12-14 21:50:21 +0900

    저녁웹개발 데이터베이스 20221214_changwoo

---
+++ practice tabe1.sql
@@ -0,0 +1,8 @@
+SELECT * FROM db_study_202210_01.score_mst;
+insert into score_mst values('박창우', '국어,수학,영어', '70,80,90', '월,수,금');
+insert into score_mst values('박창우', '국어', '70', '월');
+insert into score_mst values('박창우', '수학', '80', '수');
+insert into score_mst values('박창우', '영어', '90', '금');
+insert into score_mst values('박창우2', '국어', '50', '월');
+insert into score_mst values('박창우2', '수학', '60', '수');
+insert into score_mst values('박창우2', '영어', '100', '금');
